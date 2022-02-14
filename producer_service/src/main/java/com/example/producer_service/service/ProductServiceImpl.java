cd626b84b5aa4b8aa4d08ba99a70125aa9a826e7
neorezuss <av.bysha@gmail.com>
2022-02-14 09:23:03 +0300

    minor changes

--- producer_service/src/main/java/com/example/producer_service/service/ProductServiceImpl.java
+++ producer_service/src/main/java/com/example/producer_service/service/ProductServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.producer_service.service;
 
 import com.example.producer_service.entity.Product;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +9,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final KafkaTemplate<String, Product> kafkaTemplate;
-    @Value("${spring.kafka.template.default-topic}")
-    private String topic;
 
     @Override
     public Product sendProduct(Product product) {
-        kafkaTemplate.send(topic, product);
+        kafkaTemplate.send(
+                kafkaTemplate.getDefaultTopic(),
+                product
+        );
         return product;
     }
 }
